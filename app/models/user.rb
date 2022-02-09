c1673d6562f18f8f7aae7da112a2e1f8c8e08a9a
HayateKuronuma <hayate.kuronuma@gmail.com>
2022-02-09 17:38:13 +0900

    add: sign_upのview大枠

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,7 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable, :confirmable, :lockable, :timeoutable
+
+  validates :email, uniqueness: true
+  validates :name, uniqueness: true
 end
