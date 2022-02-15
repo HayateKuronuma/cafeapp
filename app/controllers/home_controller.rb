1ddc2b792afb357d1f8de239333490ff987c0136
HayateKuronuma <hayate.kuronuma@gmail.com>
2022-02-15 13:17:29 +0900

    add: 現在地周辺のペット可店を探す機能（ベース）
    ボタン押下で現在地取得し、さらにその周辺のペット可飲食店を取得後viewに部分更新

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,25 @@
 class HomeController < ApplicationController
+  require 'httpclient'
+
   def top
+    
+  end
+
+  def around_shops
+    @lat = params[:lat]
+    @lng = params[:lng]
+    client = HTTPClient.new
+    url = "https://webservice.recruit.co.jp/hotpepper/gourmet/v1/"
+    query = {
+      "key" => Rails.application.credentials.api[:hotpepper_api],
+      "lat" => @lat,
+      "lng" => @lng,
+      "pet" => 1,
+      "format": "json"
+    }
+    response = client.get(url, query)
+    @results = JSON.parse(response.body)
+    @shops = @results['results']['shop']
+    render partial:'ajax_shops', locals: { shops: @shops }
   end
 end
