6f7cd11a1779dc377e7c08dbf44cea78b1ce8f00
HayateKuronuma <hayate.kuronuma@gmail.com>
2022-02-19 17:58:45 +0900

    change: コントローラー名をhomeからsearchesに変更

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
-  root to: 'home#top'
-  get 'around_shops', to: 'home#around_shops'
+  root to: 'searches#top'
+  get 'around_shops', to: 'searches#around_shops'
   devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
