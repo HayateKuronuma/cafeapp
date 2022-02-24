35edeb36c4cdd58e7d9d7b2aac29dd0239563a2e
HayateKuronuma <hayate.kuronuma@gmail.com>
2022-02-24 15:16:43 +0900

    add: showアクションで店舗情報を取得
    httpclientを利用しHOT PEPPER APIからajaxでの取得

--- app/controllers/shops_controller.rb
+++ app/controllers/shops_controller.rb
@@ -1,4 +1,16 @@
 class ShopsController < ApplicationController
+  require 'httpclient'
+
   def show
+    client = HTTPClient.new
+    url = "https://webservice.recruit.co.jp/hotpepper/gourmet/v1/"
+    query = {
+      "key" => Rails.application.credentials.api[:hotpepper_api],
+      "id" => params[:shop_id],
+      "format": "json"
+    }
+    response = client.get(url, query)
+    @results = JSON.parse(response.body)
+    @shop = @results['results']['shop'][0]
   end
 end
