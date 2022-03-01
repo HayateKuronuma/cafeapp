a98fb51f9406ac58b4b53fa6572969da695c494d
HayateKuronuma <hayate.kuronuma@gmail.com>
2022-03-01 19:30:27 +0900

    add: header作成

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,10 +2,10 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable, :confirmable, :lockable, :timeoutable
+         :recoverable, :rememberable, :validatable
 
   validates :email, uniqueness: true
-  validates :name, uniqueness: true
+  validates :name, uniqueness: true, presence: true
 
   has_one_attached :avatar
 end
