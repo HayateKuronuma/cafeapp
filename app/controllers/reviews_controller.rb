6280acf86fcfe5abc73ca808f1508a26fbffb094
HayateKuronuma <hayate.kuronuma@gmail.com>
2022-03-05 13:34:00 +0900

    add: review削除機能実装

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,9 +1,25 @@
 class ReviewsController < ApplicationController
   def create
-    Review.create(review_params)
+    @review = Review.new(review_params)
     @reviews = Review.where(shop_id: params[:review][:shop_id])
+    if @review.save
+      flash.now[:notice] = "口コミを投稿しました"
+      respond_to do |format|
+        format.html { redirect_to shops_path(shop_id: params[:review][:shop_id]) }
+        format.js
+      end
+    else
+      render "error"
+    end
+  end
+
+  def destroy
+    @review = Review.find(params[:id])
+    @review_id = @review.id
+    @review.destroy
+    flash.now[:alert] = "投稿を削除しました"
     respond_to do |format|
-      format.html { redirect_to shops_path(shop_id: params[:review][:shop_id]) }
+      format.html { redirect_to shops_path(shop_id: @review.shop_id) }
       format.js
     end
   end
