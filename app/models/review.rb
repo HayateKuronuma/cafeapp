e6be9fe59f42aae0822eb45afd9c8193dd9716ac
HayateKuronuma <hayate.kuronuma@gmail.com>
2022-03-05 13:35:02 +0900

    add: review投稿時のバリデーションとエラーメッセージ表示機能実装

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,3 +1,7 @@
 class Review < ApplicationRecord
   belongs_to :user
+  validates :title, presence: true
+  validates :comment, presence: true
+  validates :user_id, presence: true
+  validates :shop_id, presence: true
 end
