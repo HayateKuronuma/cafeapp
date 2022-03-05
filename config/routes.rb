2ca22c882294672d5466f8332d07b6c62ec06ebb
HayateKuronuma <hayate.kuronuma@gmail.com>
2022-03-05 13:25:56 +0900

    add: reviews#destroyアクションルーティング

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,6 @@ Rails.application.routes.draw do
   root to: 'searches#top'
   get 'around_shops', to: 'searches#around_shops'
   devise_for :users
-  resources :reviews, only: [:create]
+  resources :reviews, only: [:create, :destroy]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
