22ba9d193213aa52f659dcb0da5edcc01b555c7f
HayateKuronuma <hayate.kuronuma@gmail.com>
2022-03-06 17:54:31 +0900

    add: rails g migration Reviewsテーブルにrateカラムを追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_03_02_232732) do
+ActiveRecord::Schema.define(version: 2022_03_06_084246) do
 
   create_table "active_storage_attachments", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.string "name", null: false
@@ -47,6 +47,7 @@ ActiveRecord::Schema.define(version: 2022_03_02_232732) do
     t.string "shop_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.float "rate", default: 0.0, null: false
   end
 
   create_table "users", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
