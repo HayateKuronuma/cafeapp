35c1d929ae7e8a415c36a9242493a6bfc485bc00
HayateKuronuma <hayate.kuronuma@gmail.com>
2022-03-08 20:43:33 +0900

    add: raty導入（星評価機能

--- app/models/review.rb
+++ app/models/review.rb
@@ -4,4 +4,8 @@ class Review < ApplicationRecord
   validates :comment, presence: true
   validates :user_id, presence: true
   validates :shop_id, presence: true
+  validates :rate, numericality: {
+    less_than_or_equal_to: 5,
+    greater_than_or_equal_to: 1
+  }, presence: true
 end
