7214cf883c7f4adc69cf3eaee9e5086befe438cf
HayateKuronuma <hayate.kuronuma@gmail.com>
2022-03-10 01:05:03 +0900

    add: reviews#edit,update追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
-  get 'shops', to: 'shops#show'
   root to: 'searches#top'
-  get 'around_shops', to: 'searches#around_shops'
   devise_for :users
-  resources :reviews, only: [:create, :destroy]
+  get 'shops', to: 'shops#show'
+  get 'around_shops', to: 'searches#around_shops'
+  resources :reviews, only: [:create, :edit, :update, :destroy]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
