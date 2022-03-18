3c54b84fe2724e82f29516fb753ba9d3f7f53b5b
HayateKuronuma <hayate.kuronuma@gmail.com>
2022-03-18 14:30:14 +0900

    add:  favorite登録上限のバリデーション設定
    hotpepperAPIの取得上限が20件のため設定

--- app/models/favorite.rb
+++ app/models/favorite.rb
@@ -1,6 +1,15 @@
 class Favorite < ApplicationRecord
+  FAVORITE_MAX_NUMBER = 20
+
   belongs_to :user
 
   validates :user_id, presence: true
   validates :shop_id, presence: true
+  validate :check_number_of_favorites
+
+  def check_number_of_favorites
+    if user && user.favorites.size >= FAVORITE_MAX_NUMBER
+      errors.add(:favorite, "登録は20件までです")
+    end
+  end
 end
