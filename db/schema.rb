1cbea25e2eea98778c696508be756acbcd374105
HayateKuronuma <hayate.kuronuma@gmail.com>
2022-03-19 19:44:51 +0900

    add: reviewテーブルにshop_nameカラム追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_03_15_141929) do
+ActiveRecord::Schema.define(version: 2022_03_19_103739) do
 
   create_table "active_storage_attachments", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.string "name", null: false
@@ -55,6 +55,7 @@ ActiveRecord::Schema.define(version: 2022_03_15_141929) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.float "rate", default: 0.0, null: false
+    t.string "shop_name", null: false
   end
 
   create_table "users", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
