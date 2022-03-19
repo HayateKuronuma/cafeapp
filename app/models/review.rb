59963f92cbb7075ffb058f25a1b32af3203f665b
HayateKuronuma <hayate.kuronuma@gmail.com>
2022-03-19 20:20:52 +0900

    add: review投稿時にshop_nameを登録するように変更
    マイレビュー一覧でショップ名を表示するため

--- app/models/review.rb
+++ app/models/review.rb
@@ -6,6 +6,7 @@ class Review < ApplicationRecord
   validates :comment, presence: true
   validates :user_id, presence: true
   validates :shop_id, presence: true
+  validates :shop_name, presence: true
   validates :rate, numericality: {
     less_than_or_equal_to: 5,
     greater_than_or_equal_to: 1
