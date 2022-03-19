af8af5326d5e764ba9b5bc0f349dddba84ed3002
HayateKuronuma <hayate.kuronuma@gmail.com>
2022-03-19 23:22:13 +0900

    add: マイレビュー一覧ページ作成

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,5 +1,10 @@
 class ReviewsController < ApplicationController
   before_action :authenticate_user!
+  before_action :where_access_from, only: [:update, :destroy]
+
+  def index
+    @my_reviews = Review.where(user_id: current_user.id)
+  end
 
   def create
     @review = Review.new(review_params)
@@ -24,6 +29,7 @@ class ReviewsController < ApplicationController
     @review = Review.find(params[:id])
     @shop_id = @review.shop_id
     @reviews = Review.where(shop_id: @shop_id).order(created_at: "DESC")
+    @my_reviews = Review.where(user_id: current_user.id)
     if @review.update(review_params)
       flash.now[:notice] = "口コミを編集しました"
       respond_to do |format|
@@ -40,6 +46,7 @@ class ReviewsController < ApplicationController
     @review_id = @review.id
     @shop_id = @review.shop_id
     @reviews = Review.where(shop_id: @shop_id).order(created_at: "DESC")
+    @my_reviews = Review.where(user_id: current_user.id)
     @review.destroy
     flash.now[:alert] = "投稿を削除しました"
     respond_to do |format|
@@ -50,6 +57,10 @@ class ReviewsController < ApplicationController
 
   private
   def review_params
-    params.require(:review).permit(:comment, :title, :shop_id, :user_id, :rate)
+    params.require(:review).permit(:comment, :title, :shop_id, :shop_name, :user_id, :rate)
+  end
+
+  def where_access_from
+    @path = Rails.application.routes.recognize_path(request.referer)
   end
 end
