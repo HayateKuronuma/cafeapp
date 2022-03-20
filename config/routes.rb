8aaeab8bb91b9ec6e88dd729fdb1fbc292565b5e
HayateKuronuma <hayate.kuronuma@gmail.com>
2022-03-20 19:15:06 +0900

    fix :ユーザー情報変更後のリダイレクト先を変更
    rails g devise:contoroller usersで作成したregistrations_contorollerにメソッドを作りオーバーライド

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,6 @@ Rails.application.routes.draw do
   resource :shop, only: [:show]
   resources :reviews, only: [:index, :create, :edit, :update, :destroy]
   resources :favorites, only: [:index, :create , :destroy]
-  devise_for :users
+  devise_for :users, controllers: { registrations: 'users/registrations' }
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
