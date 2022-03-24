c91dec2ed45d04458397b1c702358a1761901ab6
HayateKuronuma <hayate.kuronuma@gmail.com>
2022-03-25 01:40:59 +0900

    add: テストで使う共通ログイン処理をメソッド化

---
+++ spec/support/signin_support.rb
@@ -0,0 +1,22 @@
+module SigninSupport
+  module Request
+    def signed_in?
+      !session.nil?
+    end
+  end
+
+  module System
+    def log_in(user)
+      visit new_user_session_path
+
+      fill_in "user_email", with: user.email
+      fill_in "user_password", with: user.password
+      click_button "ログイン"
+    end
+  end
+end
+
+RSpec.configure do |config|
+  config.include SigninSupport::Request, type: :request
+  config.include SigninSupport::System, type: :system
+end
